d1b34594d7c2b2b375a130fce466852d6afb321b
kelseyjdickson <kelseymedved@gmail.com>
2020-07-25 20:30:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, length: { minimum: 0 }
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: { in: %w(Non-Fiction Fiction) }
 end
